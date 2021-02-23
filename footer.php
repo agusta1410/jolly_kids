8049cccb43029bb5c35a09d99b39bcdb8bdef97d
Ebenezer Isaac <mail@ebenezer-isaac.com>
2021-02-23 17:51:23 +0530

    initial commit

---
+++ footer.php
@@ -0,0 +1,14 @@
+<div id="footer">
+  <div>
+    <div> <span>Follow us</span> <a href="www.facebook.com/pages/category/TV-Network/Suga-Jeeva-TV-103170557824129" class="facebook">Facebook</a> <a href="#" class="subscribe">Subscribe</a> <a href="#" class="twitter">Twitter</a> <a href="#" class="flicker">Flickr</a> </div>
+    <ul>
+      <li> <a href="#"><img src="images/playing-in-grass.gif" alt=""></a>
+        <p>Come, you children, listen to me; I will teach you the fear of the LORD.<br>Psalms 34:11</p>
+         </li>
+      <li> <a href="#"><img src="images/baby-smiling.gif" alt=""></a>
+        <p>Behold, children are a heritage from the LORD, The fruit of the womb is a reward.<br>Psalms 127:3</p>
+         </li>
+    </ul>
+  </div>
+  <p class="footnote">Copyright &copy; 2021 <a href="#">Sugajeeva Jolly Kids</a> All rights reserved | Website Template By <a target="_blank" href="http://www.freewebsitetemplates.com/">freewebsitetemplates.com</a></p>
+</div>
